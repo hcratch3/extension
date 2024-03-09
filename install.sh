9cc3f2f849e4e90683683e172cb949e38f2d570f
hcratch3 <162408146+hcratch3@users.noreply.github.com>
2024-03-09 17:35:04 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p src/lib/libraries/extensions/extensionLoader
-cp extension.hcratch3.github.io/main/extensionLoader_entry.png src/lib/libraries/extensions/extensionLoader/
-cp extension.hcratch3.github.io/main/extensionLoader_inset.png src/lib/libraries/extensions/extensionLoader/
+cp extension.hcratch3.github.io/main/extensionLoader.png src/lib/libraries/extensions/extensionLoader/
+cp extension.hcratch3.github.io/main/extensionLoader-small.svg src/lib/libraries/extensions/extensionLoader/
+cp extension.hcratch3.github.io/main/index.jsx src/lib/libraries/extensions/extensionLoader
 mv src/lib/libraries/extensions/index.jsx src/lib/libraries/extensions/index.jsx_orig
 cp extension.hcratch3.github.io/index.jsx src/lib/libraries/extensions/index.jsx
