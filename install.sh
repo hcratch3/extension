78b01ad127111849a547dd267b3360f9a29f6dac
hcratch3 <162408146+hcratch3@users.noreply.github.com>
2024-03-20 14:47:49 +0900

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p src/lib/libraries/extensions/extensionLoader
-cp extension.hcratch3.github.io/main/extensionLoader.png src/lib/libraries/extensions/extensionLoader/
-cp extension.hcratch3.github.io/main/extensionLoader-small.svg src/lib/libraries/extensions/extensionLoader/
-cp extension.hcratch3.github.io/main/index.jsx src/lib/libraries/extensions/extensionLoader
-mv src/lib/libraries/extensions/index.jsx src/lib/libraries/extensions/index.jsx_orig
-cp extension.hcratch3.github.io/index.jsx src/lib/libraries/extensions/index.jsx
