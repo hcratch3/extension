98ece428da0f6db90bf84aca94c3e0f133b23368
hcratch3 <162408146+hcratch3@users.noreply.github.com>
2024-03-28 07:57:45 +0900

    Update index.js

--- main/dist/Gamepad/index.js
+++ main/dist/Gamepad/index.js
@@ -1,36 +1,3 @@
-const ArgumentType = require('../../extension-support/argument-type');
-const BlockType = require('../../extension-support/block-type');
-const Cast = require('../../util/cast');
-const log = require('../../util/log');
-const formatMessage = require('format-message');
-
-/**
- * Icon svg to be displayed at the left edge of each extension block, encoded as a data URI.
- * @type {string}
- */
-// eslint-disable-next-line max-len
-const blockIconURI = 'data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHdp...dGFydCIgc3R5bGU9Im1peC1ibGVuZC1tb2RlOiBub3JtYWwiPjx0c3BhbiB4PSIwIiBkeT0iMCI+R1A8L3RzcGFuPjwvdGV4dD48L2c+PC9nPjwvc3ZnPg==';
-
-/**
- * Icon svg to be displayed in the category menu, encoded as a data URI.
- * @type {string}
- */
-// eslint-disable-next-line max-len
-const menuIconURI = 'data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHdp...dGFydCIgc3R5bGU9Im1peC1ibGVuZC1tb2RlOiBub3JtYWwiPjx0c3BhbiB4PSIwIiBkeT0iMCI+R1A8L3RzcGFuPjwvdGV4dD48L2c+PC9nPjwvc3ZnPg==';
-var mPad=null;
-
-function mStartGamePad()
-{
-    window.requestAnimationFrame(mStartGamePad);
-    var gamepad_info = '';
-    var gamepads = navigator.getGamepads();
-    var gamepad_num =gamepads.length;
-    mPad = new Array(gamepad_num);
-
-    for (var i=0; i<gamepad_num; i++) {
-        if(gamepads[i]!=null) mPad[i] = gamepads[i];
-    }
-}
 class Scratch3GamePad {
 
     constructor (runtime) {
@@ -110,3 +77,4 @@ class Scratch3GamePad {
 
 }
 module.exports = Scratch3GamePad;
+
