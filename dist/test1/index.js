18fc420a19fd82b1af50939a0fd45033ad4f95af
hcratch3 <162408146+hcratch3@users.noreply.github.com>
2024-04-03 17:15:28 +0900

    Update index.js

--- dist/test1/index.js
+++ dist/test1/index.js
@@ -16,8 +16,19 @@ class Test {
     }
   }
 
-  hello() {
-    console.log('hello'); // console log に hello と出力
+  saveCode() {
+    // Scratchの現在のプロジェクトを取得
+    var project = JSON.stringify(Blockly.mainWorkspace.saveProjectSb3());
+
+    // プロジェクトをテキストファイルに保存
+    var file = new Blob([project], { type: 'text/plain' });
+    var a = document.createElement('a');
+    var url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = 'scratch_project.sb3';
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
   }
 }
 
